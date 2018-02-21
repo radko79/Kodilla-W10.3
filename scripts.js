5b3cea4a32fc1e4399dd5a99b6e93026bca85418
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-02-21 23:39:27 +0000

    Fix errors

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ var slideRight = $("#rightChevron");
 
 $(function() {
 	//this code will execute after the DOM is loaded
-	setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
-	setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
+	//setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
+	setInterval(changeSlide, 8000); //every 3 seconds, it performs a function to change the slide
 });
 
 function changeSlide() {
@@ -22,19 +22,16 @@ function moveFirstSlide() {
 	carouselList.css({marginLeft:0});
 }
 
-function moveLastSlide() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	firstItem.after(lastItem);
-	carouselList.css({marginLeft:0});
-}
-
 slideLeft.on('click', function() {
 	console.log('leftChevron');
 	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 });
 
 slideRight.on('click', function() {
+	carouselList.css({marginLeft:-400});
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	firstItem.before(lastItem);
+	carouselList.animate({'marginLeft': 0}, 500);
 	console.log('rightChevron');
-	carouselList.animate({'marginLeft': 400}, 500, moveLastSlide);
 });
